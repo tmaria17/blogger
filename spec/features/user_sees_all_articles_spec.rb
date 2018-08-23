3544d5d7aeadcdb8bc7f0b2bbaf6740c143826f0
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-08-23 15:18:00 -0600

    Adds create new article link.

--- spec/features/user_sees_all_articles_spec.rb
+++ spec/features/user_sees_all_articles_spec.rb
@@ -9,6 +9,7 @@ describe "user sees all articles" do
 
       expect(page).to have_link(article_1.title)
       expect(page).to have_link(article_2.title)
+      expect(page).to have_link("Create a New Article")
     end
   end
 end
