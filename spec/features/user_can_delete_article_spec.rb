9dd4201c2bdd3adab04409975cdf17d8eef928de
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-08-26 08:18:59 -0600

    Adds feature test and delete link to show page, Adds delete method to articles_controller.

---
+++ spec/features/user_can_delete_article_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe 'user can delete an article' do
+  describe 'they link from the show page' do
+    it 'displays all articles without deleted article' do
+      article_1 = Article.create!(title: "New Title", body: "New Body")
+      article_2 = Article.create!(title: "New Title", body: "New Body")
+      article_3 = Article.create!(title: "New Title", body: "New Body")
+      visit article_path(article_1)
+      save_and_open_page
+      click_link 'Delete'
+      expect(current_path).to eq(articles_path)
+      expect(page).to_not have_content(article_1)
+    end
+  end
+end
