07476e1967897396202b47aae8c69fe08be64924
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-08-25 20:55:57 -0600

    Adds show view for article.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,11 @@
 #app/controllers/articles_controller.rb
+
 class ArticlesController < ApplicationController
   def index
     @articles = Article.all
+  end
+
+  def show
+    @article= Article.find(params[:id])
   end
 end
